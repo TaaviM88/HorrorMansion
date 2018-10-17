f955011a99e276e843788164f1b7438b1e492b29
AKSELI-LAPTOP\Akseli <azzeli91@gmail.com>
2018-10-17 11:25:02 +0300

    Added PlayerScripts folder.

---
+++ HorroMansion-project/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Vector3 _axis;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        _axis = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        _forward = _axis.y * 
+		
+	}
+}
