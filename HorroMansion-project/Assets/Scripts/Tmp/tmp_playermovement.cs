14ff32aa6c1be0e05a893f55c542a440d0517412
TaaviM88Laptop <juomakuski@hotmail.com>
2018-10-17 11:11:55 +0300

    tmp material tmp playermovementscript

---
+++ HorroMansion-project/Assets/Scripts/Tmp/tmp_playermovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tmp_playermovement : MonoBehaviour {
+    public float speed = 50f;
+    private Rigidbody _rb;
+	// Use this for initialization
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        _rb.AddForce(movement * speed);
+	}
+}
