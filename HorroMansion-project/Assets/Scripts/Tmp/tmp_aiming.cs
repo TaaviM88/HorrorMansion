8f43eddee9d292a558fcc95cb07b731c40ded6f9
AKSELI-LAPTOP\Akseli <azzeli91@gmail.com>
2018-10-18 12:42:47 +0300

    Added and updated tmp_aiming script.

---
+++ HorroMansion-project/Assets/Scripts/Tmp/tmp_aiming.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tmp_aiming : MonoBehaviour {
+
+    public bool ifAiming = false;
+    public Transform target; 
+
+
+	// Use this for initialization
+	void Start () {
+
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ifAiming = true;
+            Vector3 targetposition = new Vector3(target.transform.position.x,
+                                                    transform.position.y,
+                                                    target.transform.position.z);
+            transform.LookAt(targetposition);
+            
+        }
+
+        if (Input.GetButtonUp("Fire2"))
+        {
+            ifAiming = false;   
+        }
+		
+	}
+}
