3b19d51ee6c7e085103a25dd7009c3d54e2ebc08
AKSELI-LAPTOP\Akseli <azzeli91@gmail.com>
2018-11-08 13:01:31 +0200

    Added AudioManager and Sound scripts.
    Added Post Processing Stack
    
    Configured graphics.

--- HorroMansion-project/Assets/Scripts/Tmp/tmp_aiming.cs
+++ HorroMansion-project/Assets/Scripts/Tmp/tmp_aiming.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class tmp_aiming : MonoBehaviour {
 
-    public bool ifAiming = false;
-    public Transform target; 
+    //public bool ifAiming = false;
+
+    public GameObject FindClosestEnemy()
+    {
+        GameObject[] _gameObjects;
+
+        _gameObjects = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject _gameObject in _gameObjects)
+        {
+            Vector3 diff = _gameObject.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = _gameObject;
+                distance = curDistance;
+                Debug.DrawLine(this.transform.position, closest.transform.position);
+            }
+        }
+        return closest;
+        
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    //public Transform target; 
 
 
 	// Use this for initialization
-	void Start () {
+	/*void Start () {
 
         
-		
+		        
 	}
 	
 	// Update is called once per frame
@@ -31,7 +62,7 @@ public class tmp_aiming : MonoBehaviour {
         if (Input.GetButtonUp("Fire2"))
         {
             ifAiming = false;   
-        }
+        }*/
 		
 	}
-}
+
