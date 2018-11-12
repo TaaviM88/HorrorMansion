debcb52827f884f1e5ce88ab60a60c9c769cf4be
TaaviM88Laptop <juomakuski@hotmail.com>
2018-11-13 01:41:24 +0200

    fire tmp particle puzzlerequired script fireplace can be now ignite in demoscene

--- HorroMansion-project/Assets/Scripts/ItemRequired.cs
+++ HorroMansion-project/Assets/Scripts/ItemRequired.cs
@@ -2,15 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemRequired : MonoBehaviour {
+public class ItemRequired : Interactable {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Equipment requiredItem;
+
+   
+    //Add this if you want spawn new item after player has "solve the puzzle"
+    //public GameObject spawnItem;
+    public GameObject spawnObject;
+    public Transform spawnpoint;
+    bool puzzleIsSolved = false;
+    public string puzzleHint = " Here cool hint for player.";
+    public string puzzleSolved = "Hey, you have already solve this.";
+
+    private void Awake()
+    {
+        spawnpoint.gameObject.SetActive(false);
+    }
+
+
+    public override void Interact()
+    {
+        base.Interact();
+        if (!puzzleIsSolved)
+        {
+            //tsekataan jos pelaajalla on 
+            Equipment e = EquipmentManager.instance.ReturnCurrentEquipment();
+            if (requiredItem.name == e.name)
+            {
+                /* GameObject newSpawnObject = Instantiate<GameObject>(spawnObject);
+                 newSpawnObject.transform.position = new Vector3(0, 0, 0);
+                 newSpawnObject.transform.parent = spawnpoint.transform;*/
+
+                spawnpoint.gameObject.SetActive(true);
+                puzzleIsSolved = true;
+                // GameObject newSpawnItem = Instantiate<GameObject>(spawnItem);
+
+            }
+            else
+            {
+                WriteToJournal(puzzleHint);
+            }
+
+        }
+        else
+            WriteToJournal(puzzleSolved);
+    }
+
+
+    private void WriteToJournal(string text)
+    {
+        Journal.Instance.Log(text);
+    }
+
 }
